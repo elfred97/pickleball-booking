db6aed164e68d9ff76ae5edceee655ce7be08949
elfred97 <elfredtapar@gmail.com>
2026-06-13 11:15:28 +0800

    connect

--- client/src/api/client.ts
+++ client/src/api/client.ts
@@ -1,4 +1,4 @@
-const API_BASE = '/api';
+const API_BASE = 'https://pickleball-booking-swll.onrender.com/api';
 
 function getAuthHeaders(): HeadersInit {
   const token = localStorage.getItem('adminToken');
